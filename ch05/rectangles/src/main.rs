04136af7ee61c9d55d4cc17ec5efcbc76aceda9c
christian-the-brewer <brewer.christiang@gmail.com>
2024-04-18 19:32:11 -0400

    completes chapter 5

--- ch05/rectangles/src/main.rs
+++ ch05/rectangles/src/main.rs
@@ -4,6 +4,18 @@ fn main() {
         height: 50,
     };
     println!("area is: {}", rect1.area());
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 13,
+        height: 60,
+    };
+
+    println!("{}",rect1.can_hold(&rect2));
+    println!("{}",rect1.can_hold(&rect3));
     }
 
     #[derive(Debug)]
@@ -15,5 +27,9 @@ fn main() {
     impl Rectangle {
         fn area(&self) -> u32 {
             self.width * self.height
+        }
+
+        fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
         }
     }
--- ch05/rectangles/target/debug/rectangles.d
+++ ch05/rectangles/target/debug/rectangles.d
@@ -1,1 +1,1 @@
-/home/christian/coding/rust/learning/rust_book/ch05/rectangles/target/debug/rectangles: /home/christian/coding/rust/learning/rust_book/ch05/rectangles/src/main.rs
+/home/gene/rust/practice/rust_book/ch05/rectangles/target/debug/rectangles: /home/gene/rust/practice/rust_book/ch05/rectangles/src/main.rs
