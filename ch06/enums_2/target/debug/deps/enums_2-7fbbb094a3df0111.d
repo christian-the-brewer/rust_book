cc05bfb420593e5072ca7b29101b1845141632d7
christian-the-brewer <brewer.christiang@gmail.com>
2024-04-24 17:34:43 -0400

    chatper 6

---
+++ ch06/enums_2/target/debug/deps/enums_2-7fbbb094a3df0111.d
@@ -0,0 +1,5 @@
+/home/gene/coding/rust/practice/rust_book/ch06/enums_2/target/debug/deps/enums_2-7fbbb094a3df0111: src/main.rs
+
+/home/gene/coding/rust/practice/rust_book/ch06/enums_2/target/debug/deps/enums_2-7fbbb094a3df0111.d: src/main.rs
+
+src/main.rs:
---
+++ ch06/enums_2/target/debug/enums_2.d
@@ -0,0 +1,1 @@
+/home/gene/coding/rust/practice/rust_book/ch06/enums_2/target/debug/enums_2: /home/gene/coding/rust/practice/rust_book/ch06/enums_2/src/main.rs
